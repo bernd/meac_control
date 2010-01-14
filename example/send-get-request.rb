7adda9a805bd372a372e18219b936674bf380a53
Bernd Ahlers <bernd@tuneafish.de>
2010-01-14 21:28:08 +0100

    Use a real options parser.

--- example/send-get-request.rb
+++ example/send-get-request.rb
@@ -3,22 +3,43 @@
 # Simple get request example.
 
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require 'ostruct'
+require 'optparse'
 require 'rubygems'
 require 'meac_control'
 require 'httpclient'
 
-host = ARGV.shift
-deviceid = ARGV.shift
+options = OpenStruct.new
+options.commands = []
 
-unless host and deviceid
-  puts "Usage: #{File.basename(__FILE__)} <ip-address> <device-id>"
+opts = OptionParser.new do |o|
+  o.banner = "Usage: #{File.basename(__FILE__)} [options]"
+
+  o.on('-i', '--ip-address IP', 'IP address of the webhost') do |value|
+    options.ip = value
+  end
+
+  o.on('-d', '--device ID', 'Device id') do |value|
+    options.device = MEACControl::Device.new(value)
+  end
+
+  o.on('--query-drive', 'Query wheter the AC is on or off') do
+    options.commands << MEACControl::Command::Drive.request
+  end
+
+  o.on('--query-fan-speed', 'Query the AC fan speed') do
+    options.commands << MEACControl::Command::FanSpeed.request
+  end
+end
+opts.parse!
+
+unless options.ip and options.device and !options.commands.empty?
+  puts opts.banner
+  puts opts.summarize
   exit 1
 end
 
-command = [MEACControl::Command::Drive.request, MEACControl::Command::FanSpeed.request]
-device = MEACControl::Device.new(deviceid)
-
-xml = MEACControl::XML::GetRequest.new(device, command)
+xml = MEACControl::XML::GetRequest.new(options.device, options.commands)
 
 puts "########### get request ###########"
 puts xml.to_xml
@@ -29,7 +50,7 @@ header = {'Accept' => 'text/xml', 'Content-Type' => 'text/xml'}
 client = HTTPClient.new
 client.protocol_version = 'HTTP/1.0'
 client.agent_name = 'meac_control/1.0'
-response = client.post("http://#{host}/servlet/MIMEReceiveServlet", xml.to_xml, header)
+response = client.post("http://#{options.ip}/servlet/MIMEReceiveServlet", xml.to_xml, header)
 
 puts "########### get response ###########"
 puts response.content
