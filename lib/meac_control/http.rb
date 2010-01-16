3d7e301b4d5301fbdea1bd9ff46ed8695e87d64b
Bernd Ahlers <bernd@tuneafish.de>
2010-01-16 14:32:44 +0100

    Add a http class to execute get and set requests via http.

---
+++ lib/meac_control/http.rb
@@ -0,0 +1,27 @@
+require 'meac_control/xml/get_request'
+require 'meac_control/xml/set_request'
+require 'meac_control/xml/response'
+require 'httpclient'
+
+module MEACControl
+  class HTTP
+    URI_TEMPLATE = 'http://%s/servlet/MIMEReceiveServlet'
+    DEFAULT_HEADER = {'Accept' => 'text/xml', 'Content-Type' => 'text/xml'}
+
+    class << self
+      def get(host, devices, commands)
+        action(host, MEACControl::XML::GetRequest.new(devices, commands))
+      end
+
+      def set(host, devices, commands)
+        action(host, MEACControl::XML::SetRequest.new(devices, commands))
+      end
+
+      private
+        def action(host, request)
+          resp = HTTPClient.post(sprintf(URI_TEMPLATE, host), request.to_xml, DEFAULT_HEADER)
+          MEACControl::XML::Response.new(resp.content)
+        end
+    end
+  end
+end
---
+++ spec/lib/meac_control/http_spec.rb
@@ -0,0 +1,68 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'spec_helper'))
+require 'meac_control/http'
+
+describe MEACControl::HTTP do
+  describe "URI_TEMPLATE constant" do
+    it "can be used to build the correct uri" do
+      uri = sprintf(MEACControl::HTTP::URI_TEMPLATE, '10.0.0.1')
+      uri.should == 'http://10.0.0.1/servlet/MIMEReceiveServlet'
+    end
+  end
+
+  describe "DEFAULT_HEADER constant" do
+    it "has 'Accept' set to 'text/xml'" do
+      MEACControl::HTTP::DEFAULT_HEADER['Accept'].should == 'text/xml'
+    end
+
+    it "has 'Content-Type' set to 'text/xml'" do
+      MEACControl::HTTP::DEFAULT_HEADER['Content-Type'].should == 'text/xml'
+    end
+  end
+
+  before(:each) do
+    @device = mock('device')
+    @command = mock('command')
+    @hostname = '127.0.0.1'
+    @uri = sprintf(MEACControl::HTTP::URI_TEMPLATE, @hostname)
+  end
+
+  describe ".get" do
+    before(:each) do
+      xml = mock('xml_request', :to_xml => fixture_read('get-request.xml'))
+
+      HTTPClient.should_receive(:post).with(@uri, xml.to_xml, MEACControl::HTTP::DEFAULT_HEADER).and_return do
+        mock('http_response', :content => fixture_read('get-response-ok.xml'))
+      end
+
+      MEACControl::XML::GetRequest.should_receive(:new).with(@device, @command).and_return(xml)
+    end
+
+    it "executes a get request" do
+      MEACControl::HTTP.get(@hostname, @device, @command)
+    end
+
+    it "returns a response object" do
+      MEACControl::HTTP.get(@hostname, @device, @command).should be_ok
+    end
+  end
+
+  describe ".set" do
+    before(:each) do
+      xml = mock('xml_request', :to_xml => fixture_read('set-request.xml'))
+
+      HTTPClient.should_receive(:post).with(@uri, xml.to_xml, MEACControl::HTTP::DEFAULT_HEADER).and_return do
+        mock('http_response', :content => fixture_read('get-response-ok.xml'))
+      end
+
+      MEACControl::XML::SetRequest.should_receive(:new).with(@device, @command).and_return(xml)
+    end
+
+    it "executes a set request" do
+      MEACControl::HTTP.set(@hostname, @device, @command)
+    end
+
+    it "returns a response object" do
+      MEACControl::HTTP.set(@hostname, @device, @command).should be_ok
+    end
+  end
+end
