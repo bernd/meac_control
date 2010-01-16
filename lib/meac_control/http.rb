da8088136f30cbd86464ff9fdb8ffd4468f8c8cf
Bernd Ahlers <bernd@tuneafish.de>
2010-01-16 15:20:53 +0100

    Document the get and set methods of the http class.

--- lib/meac_control/http.rb
+++ lib/meac_control/http.rb
@@ -9,10 +9,25 @@ module MEACControl
     DEFAULT_HEADER = {'Accept' => 'text/xml', 'Content-Type' => 'text/xml'}
 
     class << self
+      # Executes a get request and returns a MEACControl::XML::Response object.
+      #
+      # Example:
+      #   device = MEACControl::Device.new(23)
+      #   command = MEACControl::Command::Drive.request
+      #   resp = MEACControl::HTTP.get('127.0.0.1', device, command)
+      #   resp.inspect # => "#<MEACControl::XML::Response:0x8bea3ef0 ...>"
       def get(host, devices, commands)
         action(host, MEACControl::XML::GetRequest.new(devices, commands))
       end
 
+      # Executes a set request and returns a MEACControl::XML::Response object.
+      #
+      # Example:
+      #   device = MEACControl::Device.new(23)
+      #   command = MEACControl::Command::Drive.new
+      #   command.off
+      #   resp = MEACControl::HTTP.set('127.0.0.1', device, command)
+      #   resp.inspect # => "#<MEACControl::XML::Response:0x8bea3ef0 ...>"
       def set(host, devices, commands)
         action(host, MEACControl::XML::SetRequest.new(devices, commands))
       end
